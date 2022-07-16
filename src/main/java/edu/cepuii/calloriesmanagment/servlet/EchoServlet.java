6e0e1cec464d7fb2ccb382487cbc55bc44795ed9
cepuii <cepuii13@gmail.com>
2022-07-16 20:13:09 +0300

    Create servlet

---
+++ src/main/java/edu/cepuii/calloriesmanagment/servlet/EchoServlet.java
@@ -0,0 +1,27 @@
+package edu.cepuii.calloriesmanagment.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author cepuii on 16.07.2022
+ */
+@WebServlet(urlPatterns = "/users")
+public class EchoServlet extends HttpServlet {
+  
+  private static final Logger logger = LoggerFactory.getLogger(EchoServlet.class);
+  
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+    logger.info("redirect to users.jsp");
+//    req.getRequestDispatcher("users.jsp").forward(req,resp);
+    resp.sendRedirect("users.jsp");
+  }
+}
