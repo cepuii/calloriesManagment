aec71f9496709a3d44cc1d3fed441b85bb121aae
cepuii <cepuii13@gmail.com>
2022-07-28 23:28:12 +0300

    Refactoring repositories

--- src/main/java/edu/cepuii/calloriesmanagement/repository/inmemory/InMemoryMealRepository.java
+++ src/main/java/edu/cepuii/calloriesmanagement/repository/inmemory/InMemoryMealRepository.java
@@ -1,13 +1,21 @@
 package edu.cepuii.calloriesmanagement.repository.inmemory;
 
+import static edu.cepuii.calloriesmanagement.repository.inmemory.InMemoryUserRepository.ADMIN_ID;
+import static edu.cepuii.calloriesmanagement.repository.inmemory.InMemoryUserRepository.USER_ID;
+
 import edu.cepuii.calloriesmanagement.model.Meal;
 import edu.cepuii.calloriesmanagement.repository.MealRepository;
 import edu.cepuii.calloriesmanagement.util.MealUtil;
+import edu.cepuii.calloriesmanagement.util.Util;
+import java.time.LocalDateTime;
+import java.time.Month;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -18,55 +26,58 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public class InMemoryMealRepository implements MealRepository {
+  
   private static final Logger log = LoggerFactory.getLogger(InMemoryMealRepository.class);
-  private final static AtomicInteger generateId = new AtomicInteger(1);
-  private final Map<Integer, Meal> repository = new ConcurrentHashMap<>();
+  private final Map<Integer, InMemoryBaseRepository<Meal>> usersMealsMap = new ConcurrentHashMap<>();
+  private final static AtomicInteger generateId = new AtomicInteger(0);
   
   {
-    MealUtil.getMeals().forEach(meal -> save(meal, 1));
+    MealUtil.getMeals().forEach(meal -> save(meal, USER_ID));
+    save(new Meal(LocalDateTime.of(2015, Month.JUNE, 1, 14, 0), "Админ ланч", 510), ADMIN_ID);
+    save(new Meal(LocalDateTime.of(2015, Month.JUNE, 1, 21, 0), "Админ ужин", 1500), ADMIN_ID);
+    
   }
   
   @Override
   public Meal save(Meal meal, int userId) {
-    if (meal.isNew()) {
-      meal.setId(generateId.incrementAndGet());
-      meal.setUserId(userId);
-      log.info("create and add new meal, id:" + meal.getId() + " by user: " + userId);
-      repository.put(meal.getId(), meal);
-      return meal;
-    }
-    log.info(" update meal, id: " + meal.getId() + " by user: " + userId);
-    if (repository.get(meal.getId()).getUserId() != userId) {
-      return null;
-    }
-    return repository.computeIfPresent(meal.getId(), (id, oldMeal) -> meal);
+    InMemoryBaseRepository<Meal> meals = usersMealsMap.computeIfAbsent(userId,
+        uId -> new InMemoryBaseRepository<>());
+    return meals.save(meal);
   }
   
   @Override
   public boolean delete(int id, int userId) {
     log.info("delete meal, id: " + id + " by user: " + userId);
-    Meal meal = getById(id, userId);
-    if (meal == null) {
-      return false;
-    }
-    return repository.remove(id, meal);
+    InMemoryBaseRepository<Meal> meals = usersMealsMap.get(userId);
+    return meals != null && meals.delete(id);
+  }
+  
+  @Override
+  public Meal get(int id, int userId) {
+    log.info("get meal, id: " + id + " by user: " + userId);
+    InMemoryBaseRepository<Meal> meals = usersMealsMap.get(userId);
+    return meals == null ? null : meals.get(id);
+  }
+  
+  public Collection<Meal> getBetweenHalfOpen(LocalDateTime start, LocalDateTime end, int userId) {
+    return filterByPredicate(userId,
+        meal -> Util.isBetweenHalfOpen(meal.getDateTime(), start, end));
   }
   
   @Override
   public Collection<Meal> getAll(int userId) {
-    log.info("get All Meals, sorted by date/time descending");
-    return repository.values().stream()
-        .filter(meal -> meal.getUserId() == userId)
-        .sorted(Comparator.comparing(Meal::getDateTime).reversed())
-        .collect(Collectors.toList());
+    return filterByPredicate(userId, meal -> true);
   }
   
-  @Override
-  public Meal getById(int id, int userId) {
-    log.info("get meal, id: " + id + " by user: " + userId);
-    if (userId != repository.get(id).getUserId()) {
-      return null;
-    }
-    return repository.get(id);
+  public Collection<Meal> filterByPredicate(int userId, Predicate<Meal> filter) {
+    log.info("get All Meals, sorted by date/time descending");
+    InMemoryBaseRepository<Meal> meals = usersMealsMap.get(userId);
+    return meals == null ? Collections.emptyList() :
+        meals.getCollection().stream()
+            .filter(filter)
+            .sorted(Comparator.comparing(Meal::getDateTime).reversed())
+            .collect(Collectors.toList());
   }
+  
+  
 }
--- src/main/java/edu/cepuii/calloriesmanagement/repository/inmemory/InMemoryUserRepository.java
+++ src/main/java/edu/cepuii/calloriesmanagement/repository/inmemory/InMemoryUserRepository.java
@@ -3,9 +3,8 @@ package edu.cepuii.calloriesmanagement.repository.inmemory;
 import edu.cepuii.calloriesmanagement.model.User;
 import edu.cepuii.calloriesmanagement.repository.UserRepository;
 import java.util.Collection;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.Comparator;
+import java.util.stream.Collectors;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
@@ -14,50 +13,27 @@ import org.springframework.stereotype.Repository;
  * @author cepuii on 22.07.2022
  */
 @Repository
-public class InMemoryUserRepository implements UserRepository {
+public class InMemoryUserRepository extends InMemoryBaseRepository<User> implements UserRepository {
   
   private static final Logger log = LoggerFactory.getLogger(InMemoryUserRepository.class);
   
-  private final static AtomicInteger generateId = new AtomicInteger(1);
-  
-  private final Map<Integer, User> repository = new ConcurrentHashMap<>();
-  
-  @Override
-  public User save(User user) {
-    if (user.isNew()) {
-      user.setId(generateId.getAndIncrement());
-      log.info("add new user, id: " + user.getId());
-      repository.put(user.getId(), user);
-      return user;
-    }
-    log.info("update user, id: " + user.getId());
-    return repository.computeIfPresent(user.getId(), (id, oldUser) -> user);
-  }
-  
-  @Override
-  public boolean delete(int id) {
-    log.info("delete user, id: " + id);
-    return repository.remove(id, get(id));
-  }
-  
-  @Override
-  public User get(int id) {
-    log.info("get user, id: " + id);
-    return repository.get(id);
-  }
+  static final int USER_ID = 1;
+  static final int ADMIN_ID = 2;
   
   @Override
   public User getByEmail(String email) {
     log.info("get by mail " + email);
-    return repository.values().stream()
+    return getCollection().stream()
         .filter(user -> user.getEmail().equals(email))
-        .findAny()
-        .get();
+        .findFirst()
+        .orElse(null);
   }
   
   @Override
   public Collection<User> getAll() {
     log.info("get all");
-    return repository.values();
+    return getCollection().stream()
+        .sorted(Comparator.comparing(User::getName).thenComparing(User::getEmail))
+        .collect(Collectors.toList());
   }
 }
