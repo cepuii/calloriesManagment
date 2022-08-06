c583d1cd41920e8af81b8d89aea4ead06fdc8290
cepuii <cepuii13@gmail.com>
2022-08-07 01:55:48 +0300

    Add meals data to database

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS user_roles;
 DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS meals;
 DROP SEQUENCE IF EXISTS global_seq;
 
 CREATE SEQUENCE global_seq START WITH 100000;
@@ -16,6 +17,19 @@ CREATE TABLE users
 );
 
 CREATE UNIQUE INDEX users_unique_email_idx ON users (email);
+
+CREATE TABLE meals
+(
+    id          INTEGER PRIMARY KEY DEFAULT nextval('global_seq'),
+    date_time   TIMESTAMP           DEFAULT now() NOT NULL,
+    description VARCHAR,
+    calories    INTEGER,
+    user_id     INTEGER                           NOT NULL,
+    CONSTRAINT user_id_date_time UNIQUE (user_id, date_time),
+    FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
+);
+
+CREATE UNIQUE INDEX meals_unique_user_datetime_idx ON meals (date_time);
 
 CREATE TABLE user_roles
 (
