b4f105f98e70b62b1fb274c5197f73d21f44cd8d
cepuii <cepuii13@gmail.com>
2022-08-06 23:05:53 +0300

    Override equals and hashCode

--- src/main/java/edu/cepuii/calloriesmanagement/model/AbstractBaseEntity.java
+++ src/main/java/edu/cepuii/calloriesmanagement/model/AbstractBaseEntity.java
@@ -1,11 +1,18 @@
 package edu.cepuii.calloriesmanagement.model;
 
+import java.util.Objects;
+
 /**
  * @author cepuii on 19.07.2022
  */
 public abstract class AbstractBaseEntity {
   
+  public static final int START_SEQ = 100000;
+  
   protected Integer id;
+  
+  public AbstractBaseEntity() {
+  }
   
   public AbstractBaseEntity(Integer id) {
     this.id = id;
@@ -21,6 +28,23 @@ public abstract class AbstractBaseEntity {
   
   public boolean isNew() {
     return id == null;
+  }
+  
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (o == null || getClass() != o.getClass()) {
+      return false;
+    }
+    AbstractBaseEntity that = (AbstractBaseEntity) o;
+    return Objects.equals(id, that.id);
+  }
+  
+  @Override
+  public int hashCode() {
+    return id == null ? 0 : id;
   }
   
   @Override
