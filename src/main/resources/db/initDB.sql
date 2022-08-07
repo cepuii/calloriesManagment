c27f0ae0a09203cf390eae797b44c7edc2e784a0
cepuii <cepuii13@gmail.com>
2022-08-07 16:58:09 +0300

    Add meals data to database

--- src/main/resources/db/initDB.sql
+++ src/main/resources/db/initDB.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS user_roles;
-DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS meals;
+DROP TABLE IF EXISTS users;
 DROP SEQUENCE IF EXISTS global_seq;
 
 CREATE SEQUENCE global_seq START WITH 100000;
@@ -29,7 +29,7 @@ CREATE TABLE meals
     FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
 
-CREATE UNIQUE INDEX meals_unique_user_datetime_idx ON meals (date_time);
+CREATE INDEX meals_unique_user_datetime_idx ON meals USING btree (date_time);
 
 CREATE TABLE user_roles
 (
