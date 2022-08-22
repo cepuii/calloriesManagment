9ea0c1df53a47b5019545aee1a2b0f62c589d4c7
Serhiy Chernousov <cepuii13@gmail.com>
2022-08-23 00:25:01 +0300

    Implement meal repo by Spring Data JPA

--- src/main/java/edu/cepuii/caloriesmanagment/repository/dataJpa/CrudMealRepository.java
+++ src/main/java/edu/cepuii/caloriesmanagment/repository/dataJpa/CrudMealRepository.java
@@ -1,10 +1,25 @@
 package edu.cepuii.caloriesmanagment.repository.dataJpa;
 
 import edu.cepuii.caloriesmanagment.model.Meal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional(readOnly = true)
 public interface CrudMealRepository extends JpaRepository<Meal, Integer> {
-
+  
+  @Transactional
+  @Modifying
+  int deleteByIdAndUser_Id(int id, int userId);
+  
+  Optional<Meal> findByIdAndUser_Id(int id, int userId);
+  
+  List<Meal> findAllByUser_IdOrderByDateTimeDesc(int userId);
+  
+  
+  List<Meal> findAllByUser_IdAndAndDateTimeAfterAndAndDateTimeBeforeOrderByDateTimeDesc(int userId,
+      LocalDateTime start, LocalDateTime end);
 }
