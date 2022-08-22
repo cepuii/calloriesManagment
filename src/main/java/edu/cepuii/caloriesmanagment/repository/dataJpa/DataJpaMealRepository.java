210894db39714765c05fdc025f16e20790aff771
Serhiy Chernousov <cepuii13@gmail.com>
2022-08-23 00:25:01 +0300

    Implement meal repo by Spring Data JPA

--- src/main/java/edu/cepuii/caloriesmanagment/repository/dataJpa/DataJpaMealRepository.java
+++ src/main/java/edu/cepuii/caloriesmanagment/repository/dataJpa/DataJpaMealRepository.java
@@ -9,34 +9,42 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class DataJpaMealRepository implements MealRepository {
   
+  private final CrudMealRepository mealRepository;
   private final CrudUserRepository userRepository;
   
-  public DataJpaMealRepository(CrudUserRepository userRepository) {
+  public DataJpaMealRepository(CrudMealRepository mealRepository,
+      CrudUserRepository userRepository) {
+    this.mealRepository = mealRepository;
     this.userRepository = userRepository;
   }
   
   @Override
   public Meal save(Meal meal, int userId) {
-    return null;
+    if (!meal.isNew() && get(meal.getId(), userId) == null) {
+      return null;
+    }
+    meal.setUser(userRepository.getReferenceById(userId));
+    return mealRepository.save(meal);
   }
   
   @Override
   public boolean delete(int id, int userId) {
-    return false;
+    return mealRepository.deleteByIdAndUser_Id(id, userId) != 0;
   }
   
   @Override
   public Meal get(int id, int userId) {
-    return null;
+    return mealRepository.findByIdAndUser_Id(id, userId).orElse(null);
   }
   
   @Override
   public Collection<Meal> getAll(int userId) {
-    return null;
+    return mealRepository.findAllByUser_IdOrderByDateTimeDesc(userId);
   }
   
   @Override
   public Collection<Meal> getBetweenHalfOpen(LocalDateTime start, LocalDateTime end, int userId) {
-    return null;
+    return mealRepository.findAllByUser_IdAndAndDateTimeAfterAndAndDateTimeBeforeOrderByDateTimeDesc(
+        userId, start, end);
   }
 }
