ac2e898f01e12672b48a60399fcefe2bae0efbfb
Serhiy Chernousov <cepuii13@gmail.com>
2022-08-23 00:55:39 +0300

    Add spring profiles DataJpa, Jpa, JDBC
    Default chose DataJpa

--- src/test/java/edu/cepuii/caloriesmanagment/SpringMain.java
+++ src/test/java/edu/cepuii/caloriesmanagment/SpringMain.java
@@ -1,13 +1,18 @@
 package edu.cepuii.caloriesmanagment;
 
+import static edu.cepuii.caloriesmanagment.Profiles.ACTIVE_DB;
+import static edu.cepuii.caloriesmanagment.Profiles.REPOSITORY_IMPLEMENTATION;
+
 import edu.cepuii.caloriesmanagment.web.meal.MealRestController;
 import java.util.Arrays;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
 
 /**
  * @author cepuii on 22.07.2022
  */
+@ActiveProfiles({ACTIVE_DB, REPOSITORY_IMPLEMENTATION})
 public class SpringMain {
   
   public static void main(String[] args) {
