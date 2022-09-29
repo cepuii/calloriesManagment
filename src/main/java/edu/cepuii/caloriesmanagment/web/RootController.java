afc23d0af2b30297f0fa27dfdc8dc5daee86a991
Serhiy Chernousov <cepuii13@gmail.com>
2022-09-30 00:27:44 +0300

    Add necessary annotation

--- src/main/java/edu/cepuii/caloriesmanagment/web/RootController.java
+++ src/main/java/edu/cepuii/caloriesmanagment/web/RootController.java
@@ -4,17 +4,20 @@ import edu.cepuii.caloriesmanagment.service.UserService;
 import javax.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@Controller
 public class RootController {
   
   private static final Logger log = LoggerFactory.getLogger(RootController.class);
+  private final UserService service;
   
-  @Autowired
-  private UserService service;
+  public RootController(UserService service) {
+    this.service = service;
+  }
   
   @GetMapping("/")
   public String root() {
